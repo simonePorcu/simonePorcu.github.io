0e7e7fcf351f55abd3616b173fac01f3f6a4af14
simonePorcu <60370548+simonePorcu@users.noreply.github.com>
2020-01-28 11:17:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+const drone = new ScaleDrone('O50F6ChH5cu3t166');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
