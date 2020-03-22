ca0821c4a1b459f5513cdb9b803848f6eb90b6b2
simonePorcu <60370548+simonePorcu@users.noreply.github.com>
2020-03-22 22:03:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const drone = new ScaleDrone('O50F6ChH5cu3t166');
 const roomName = 'observable-' + roomHash;
 const configuration = {
   iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
+    urls: 'stun:stunserver.example.org'
+    
   }]
 };
 let room;
